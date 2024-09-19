347c8b8d5560af68949c0d6c5cdc91049ab3cde5
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-19 18:04:11 +0530

    Create products model using mongoose schema

---
+++ models/productModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const productSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    unique: true,
+    required: [true, "A product must have a name"],
+  },
+  productMaterial: {
+    type: String,
+  },
+  productCatogary: {
+    type: String,
+    required: [true, "A product must be inside a category"],
+  },
+
+  description: {
+    type: String,
+  },
+  pricePerUnit: {
+    type: Number,
+    required: [true, "A product must have a price"],
+  },
+  stockAvailable: {
+    type: Number,
+    validate: {
+      validator: function (val) {
+        return val <= 0;
+      },
+      message: "Stocks cannot be in negative",
+    },
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
