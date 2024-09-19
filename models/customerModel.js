bca1be7f45a1fc769433be9f20982f2787985d6f
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-19 17:49:04 +0530

    Create customer Model using mongoose schema

---
+++ models/customerModel.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const customerSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please provide a last name"],
+    },
+    address: {
+      street: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      country: {
+        type: String,
+      },
+    },
+    phone: {
+      type: String,
+      required: [true, "Please provide a phone number"],
+    },
+    source: {
+      type: String,
+      required: [true, "Please provide a source"],
+    },
+    joinedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    lastLogin: {
+      type: Date,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide an email"],
+      validate: [validator.isEmail, "Please provide a valid email address"],
+    },
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+customerSchema.virtual("status").get(function () {
+  const now = Date.now();
+  const last30Days = new Date(now - 30 * 24 * 60 * 60 * 1000);
+
+  if (this.joinedAt >= last30Days) {
+    return "new";
+  } else if (this.lastLogin >= last30Days) {
+    return "active";
+  } else {
+    return "inactive";
+  }
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
