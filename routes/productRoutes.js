22a5affa1003f02533b423d93092ab55c08e8cd5
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-22 16:33:00 +0530

    Add product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,5 +5,6 @@ const productController = require("./../controllers/productController");
 const router = express.Router();
 
 router.route("/").get(productController.getAllProducts);
+router.route("/:productId").get(productController.getProduct);
 
 module.exports = router;
