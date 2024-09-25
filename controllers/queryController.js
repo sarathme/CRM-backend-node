2ac04fa26360e73d30bd24c6368cd698c0796fe3
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-26 01:19:49 +0530

    Implement routes and handler function for query stats

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -3,15 +3,57 @@ const Query = require("./../models/queryModel");
 const APIFeatures = require("./../utils/apiFeatures");
 
 exports.getAllQueries = catchAsync(async (req, res, next) => {
-  const totalqueries = await Query.countDocuments();
-  const features = new APIFeatures(Query.find(), req.query).paginate();
-  const queries = await features.query;
+  if (!req.query.page || !req.query.limit) {
+    req.query.page = req.query.page || 1;
+    req.query.limit = req.query.limit || 20;
+  }
+
+  const query = Query.find();
+  const totalqueries = await Query.countDocuments(
+    new APIFeatures(query, req.query).filter().query
+  );
+  const features = new APIFeatures(query, req.query).filter().paginate();
+  const queries = await features.query
+    .populate({
+      path: "customerId",
+      select: "firstName lastName",
+    })
+    .populate({
+      path: "productId",
+      select: "productName",
+    });
   res.status(200).json({
     status: "success",
     totalqueries,
-    totalPages: Math.ceil(totalqueries / 20),
+    totalPages: Math.ceil(totalqueries / req.query.limit),
     data: {
       queries,
+    },
+  });
+});
+
+exports.getQueryStats = catchAsync(async (req, res, next) => {
+  const stats = await Query.aggregate([
+    {
+      $group: {
+        _id: "$queryType",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+  const stats2 = await Query.aggregate([
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+  res.status(200).json({
+    status: "success",
+    data: {
+      stats,
+      stats2,
     },
   });
 });
--- routes/queryRoutes.js
+++ routes/queryRoutes.js
@@ -8,5 +8,6 @@ const queryController = require("./../controllers/queryController");
 const router = express.Router();
 
 router.route("/").get(queryController.getAllQueries);
+router.route("/stats").get(queryController.getQueryStats);
 
 module.exports = router;
